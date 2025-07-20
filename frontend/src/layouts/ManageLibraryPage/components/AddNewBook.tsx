f6e874fd4802246d13ca3df92adab839bf7966c2
Ngô Quang Trường <ngoquangtruong.isme@gmail.com>
2025-07-20 22:52:56 +0700

    Fix fe

--- frontend/src/layouts/ManageLibraryPage/components/AddNewBook.tsx
+++ frontend/src/layouts/ManageLibraryPage/components/AddNewBook.tsx
@@ -92,17 +92,17 @@ export const AddNewBook = () => {
                 <div className='card-body'>
                     <form method='POST'>
                         <div className='row'>
-                            <div className='col-12 col-md-6 mb-3'>
+                            <div className='col-md-6 mb-3'>
                                 <label className='form-label'>Title</label>
                                 <input type="text" className='form-control' name='title' required 
                                     onChange={e => setTitle(e.target.value)} value={title} />
                             </div>
-                            <div className='col-12 col-md-3 mb-3'>
+                            <div className='col-md-3 mb-3'>
                                 <label className='form-label'> Author </label>
                                 <input type="text" className='form-control' name='author' required 
                                     onChange={e => setAuthor(e.target.value)} value={author}/>
                             </div>
-                            <div className='col-12 col-md-3 mb-3'>
+                            <div className='col-md-3 mb-3'>
                                 <label className='form-label'> Category</label>
                                 <button className='form-control btn btn-secondary dropdown-toggle' type='button' 
                                     id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false'>
@@ -126,7 +126,7 @@ export const AddNewBook = () => {
                             <input type='number' className='form-control' name='Copies' required 
                                 onChange={e => setCopies(Number(e.target.value))} value={copies}/>
                         </div>
-                        <input type='file' onChange={e => base64ConversionForImages(e)}/>
+                        <input type='file' className='form-control w-100' onChange={e => base64ConversionForImages(e)}/>
                         <div>
                             <button type='button' className='btn btn-primary mt-3' onClick={submitNewBook}>
                                 Add Book
